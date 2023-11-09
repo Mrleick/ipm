e37d0d5148c8bab93fafe5ea81f949aea61ac036
Mathiassren <110809330+Mathiassren@users.noreply.github.com>
2023-11-09 12:26:11 +0100

    Made frame motion on welcomepage

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -3,11 +3,8 @@ import styled, { css } from "styled-components";
 const StyledHeading = styled((props) => props.as)`
   font-size: ${(props) => props.size}px;
   color: ${(props) => props.color};
-  ${(props) =>
-    props.as === "h1" &&
-    css`
-      font-weight: bold;
-    `}
+  font-weight: ${(props) => props.weight};
+  ${(props) => props.as === "h1" && css``}
 `;
 
 const Heading = (props) => {
