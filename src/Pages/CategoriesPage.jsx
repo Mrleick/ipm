6b459506aa7b1c07197796ccf5403a425c021a07
kpauner <kpauner@gmail.com>
2023-11-09 11:30:43 +0100

    categories and framer-motion

--- src/Pages/CategoriesPage.jsx
+++ src/Pages/CategoriesPage.jsx
@@ -1,10 +1,17 @@
 import { Link } from "react-router-dom";
+import Accordion from "../components/Accordion";
 
 const CatagoriesPage = () => {
   return (
     <>
       <h1>Catagories</h1>
       <Link to="/Songs">Songs</Link>
+      <main>
+        <Accordion
+          question="hello"
+          answer="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        />
+      </main>
     </>
   );
 };
---
+++ src/components/Accordion.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+const Accordion = ({ question, answer }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <motion.div>
+      <AnimatePresence>
+        <motion.div
+          key="question"
+          className="rounded-tr-md relative z-20 bg-red rounded-br-md shadow-sm px-1 py-2 bg-blue-200 cursor-pointer font-open border-l-2 border-yellow-500"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <motion.div className="text-gray-800 font-bold ml-1">
+            {question}
+          </motion.div>
+        </motion.div>
+
+        {isOpen && (
+          <motion.div
+            key="answer"
+            initial={{ opacity: 0 }}
+            animate={{
+              opacity: 1,
+              transition: {
+                duration: 0.5,
+              },
+            }}
+            exit={{ opacity: 0 }}
+            className="p-2 text-lg text-gray-700 border-l border-b border-gray-300"
+          >
+            {answer}
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </motion.div>
+  );
+};
+
+export default Accordion;
