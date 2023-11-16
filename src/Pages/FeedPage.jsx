c015086dc40d173bdfc2fafdf7e82dd95c56eae5
Mathiassren <110809330+Mathiassren@users.noreply.github.com>
2023-11-16 15:56:35 +0100

    feedpage updated

--- src/Pages/FeedPage.jsx
+++ src/Pages/FeedPage.jsx
@@ -1,10 +1,24 @@
-import { Link } from "react-router-dom";
+import tw from "tailwind-styled-components";
+import Card from "../components/Feedcard";
+import Categories from "../components/Feedcategories";
+import Backbtn from "../components/Backbtn";
+
+const Container = tw.section`
+  bg-secondary-color
+  h-screen
+  pl-5
+  pr-5
+`;
 
 const FeedPage = () => {
   return (
     <>
-      <h1>Feed</h1>
-      <Link to="/Trends">Trends</Link>
+      <Container>
+        <Backbtn />
+        <h1 className="text-white font-bold">Events Feed</h1>
+        <Categories />
+        <Card />
+      </Container>
     </>
   );
 };
---
+++ src/components/Feedgroup.jsx
@@ -0,0 +1,27 @@
+import tw from "tailwind-styled-components";
+import roundpexel from "../assets/1.png";
+import roundpexel2 from "../assets/2.png";
+import roundpexel3 from "../assets/3.png";
+
+const Feedgroup = () => {
+  return (
+    <div className="flex items-center text-white">
+      <div className="">
+        <img src={roundpexel} alt="Image 1" className="w-full h-auto" />
+      </div>
+      <div className="-ml-8">
+        <img src={roundpexel2} alt="Image 2" className="w-full h-auto" />
+      </div>
+      <div className="-ml-8">
+        <img src={roundpexel3} alt="Image 3" className="w-full h-auto" />
+      </div>
+      <div className="">
+        <p className="text-lg">
+          <span className="font-bold">3,123 </span>are talking about this
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Feedgroup;
