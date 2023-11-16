03ecc6d1f46f73a6570200de77d1a0bd4a5448c7
Daniel Christiansen <117975547+XENONDK@users.noreply.github.com>
2023-11-16 11:03:25 +0100

    albums updata

--- src/Pages/AlbumsPage.jsx
+++ src/Pages/AlbumsPage.jsx
@@ -1,12 +1,12 @@
 // import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import FooterMenu from "../components/FooterMenu";
-import { IoIosArrowBack, IoIosSearch } from "react-icons/io";
 import Header from "../components/Header";
+
 
 const AlbumPage = () => {
   const [token, setToken] = useState();
-
+////////////////////////// Get Token ////////////////////////////
   useEffect(() => {
     const body = new URLSearchParams({
       grant_type: "client_credentials",
@@ -26,36 +26,46 @@ const AlbumPage = () => {
 
   const [songs, setSongs] = useState();
 
+  //////////////////////////7 get singles offset /////////////////////////
   useEffect(() => {
     fetch(
-      "https://api.spotify.com/v1/albums/6JoI0NEAqeJ20X6lU3Drx0?si=14IqgkiyS7-cSp5_Nv7GMQ/tracks",
+      "https://api.spotify.com/v1/browse/new-releases?offset=20",
       {
         method: "GET",
         headers: {
           "content-type": "application/x-www-form-urlencoded",
-          Authorization: `Bearer ${token} `,
+          Authorization: `Bearer ${token}`,
         },
       }
     )
       .then((res) => res.json())
-      .then((data) => setSongs(data));
+      .then((data) => setSongs(data.albums.items));
   }, [token]);
 
+  ///////////////////// get Featured Albums /////////////////////
+  const [FeatAlbums, setFeatAlbums] = useState()
+  useEffect(() => {
+    fetch(
+      "https://api.spotify.com/v1/browse/new-releases",
+      {
+        method: "GET",
+        headers: {
+          "content-type": "application/x-www-form-urlencoded",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => setFeatAlbums(data.albums.items));
+  },[token])
+
+    
   return (
     <>
       <div>
         <div className="w-screen h-screen bg-primarycolor dark:bg-white">
           <Header />
-          <header className="pt-5 px-5 flex justify-between text-white dark:text-black">
-            <button className="dark:text-black text-white text-2xl">
-              <IoIosArrowBack />
-            </button>
-            <h2>Music</h2>
-            <button className="dark:text-black text-white text-2xl">
-              <IoIosSearch />
-            </button>
-          </header>
-
+          
           <section>
             <h1 className="bg-gradient-to-r from-[#FF6A00] to-[#EE0979] text-transparent bg-clip-text text-5xl pl-5 py-7">
               All Albums
@@ -71,42 +81,18 @@ const AlbumPage = () => {
             </div>
 
             <section className="gap-5 flex overflow-x-auto px-5 pb-8">
-              <img
-                src={songs && songs.images[0].url}
+              {FeatAlbums && FeatAlbums.map((album) => (
+                <img
+                src={album.images[0].url}
                 className="rounded-lg h-32"
-              />
-              <img
-                src={songs && songs.images[0].url}
-                className="rounded-lg h-32"
-              />
-              <img
-                src={songs && songs.images[0].url}
-                className="rounded-lg h-32"
-              />
-              <img
-                src={songs && songs.images[0].url}
-                className="rounded-lg h-32"
-              />
-              <img
-                src={songs && songs.images[0].url}
-                className="rounded-lg h-32"
-              />
-              <img
-                src={songs && songs.images[0].url}
-                className="rounded-lg h-32"
-              />
-              <img
-                src={songs && songs.images[0].url}
-                className="rounded-lg h-32"
-              />
-              <img
-                src={songs && songs.images[0].url}
-                className="rounded-lg h-32"
-              />
-              <img
-                src={songs && songs.images[0].url}
-                className="rounded-lg h-32"
-              />
+                key={album.id}
+              /> ) 
+              )
+              }
+
+
+               
+              
             </section>
 
             <div className="flex justify-between px-5 pb-4 text-white dark:text-black">
@@ -118,26 +104,26 @@ const AlbumPage = () => {
                 View more
               </a>
             </div>
-            <section className="overflow-y-auto max-h-96 pb-14">
+            <section className="overflow-y-auto h-40 pb-14">
               {songs &&
-                songs.tracks.items.map((track) => (
+                songs.map((single) => (
                   <article
                     className="flex px-5 justify-between items-center mb-6 text-white dark:text-black"
-                    key={track.id}
+                    key={single.id}
                   >
                     <div className="flex gap-3">
                       <img
-                        src={songs && songs.images[0].url}
+                        src={single.images[0].url}
                         className="h-14 rounded-lg"
                       />
                       <span className="max-h-14">
                         <p className="font-bold text-xl m-0">
-                          {track.name.length > 15
-                            ? track.name.split(" ").slice(0, 3).join(" ") +
+                          {single.name.length > 15
+                            ? single.name.split(" ").slice(0, 3).join(" ") +
                               "..."
-                            : track.name}
+                            : single.name}
                         </p>
-                        <p className="m-0">{track.artists[0].name}</p>
+                        <p className="m-0">{single.artists[0].name}</p>
                       </span>
                     </div>
                     <p>12 sec</p>
