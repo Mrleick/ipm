8005cd6042fe4d58c109922d72a4a285a4af8301
Mathiassren <110809330+Mathiassren@users.noreply.github.com>
2023-11-16 16:52:08 +0100

    deleted backbtns

--- src/Pages/FeedPage.jsx
+++ src/Pages/FeedPage.jsx
@@ -2,23 +2,25 @@ import tw from "tailwind-styled-components";
 import Card from "../components/Feedcard";
 import Categories from "../components/Feedcategories";
 import Backbtn from "../components/Backbtn";
+import Heading from "../components/Heading";
 
 const Container = tw.section`
   bg-secondary-color
   h-screen
-  pl-5
-  pr-5
+  px-6 pb-8
 `;
 
 const FeedPage = () => {
   return (
     <>
-      <Container>
-        <Backbtn />
-        <h1 className="text-white font-bold">Events Feed</h1>
-        <Categories />
-        <Card />
-      </Container>
+      <main>
+        <Container>
+          <Backbtn />
+          <Heading title="Featured" />
+          <Categories />
+          <Card />
+        </Container>
+      </main>
     </>
   );
 };
