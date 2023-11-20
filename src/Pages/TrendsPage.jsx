53759ee728c82fa2f885bc9ffb1a3925f39c256a
Daniel Christiansen <117975547+XENONDK@users.noreply.github.com>
2023-11-20 14:12:26 +0100

    Update TrendsPage.jsx

--- src/Pages/TrendsPage.jsx
+++ src/Pages/TrendsPage.jsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
+import Header from "../components/Header";
+import FooterMenu from "../components/FooterMenu"
+import Heading from "../components/Heading";
 
 const TrendsPage = () => {
   return (
     <>
-      <h1>Trends</h1>
-      <Link to="/Playlist">Playlist</Link>
+      <Header />
+      <Heading title="Latest Trends" level="1" className="text-3xl px-5 py-0"/>
+      <FooterMenu />
     </>
   );
 };
