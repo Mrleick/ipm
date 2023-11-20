5222404cc3759d7f4f38aa29b278d4c88e1842b3
NickBLau <114568331+NickBLau@users.noreply.github.com>
2023-11-20 12:42:59 +0100

    Added Content and styling to AlbumDetails

--- src/Pages/AlbumDetailsPage.jsx
+++ src/Pages/AlbumDetailsPage.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Heading from "../components/Heading";
 import fetchFromApi from "../lib/fetchFromApi";
@@ -23,6 +24,15 @@ h-4
 text-white
 `;
 
+const StyledButton = tw.button`
+bg-primarycolor
+ text-base
+h-10
+w-fit
+px-3
+   rounded-3xl
+`;
+
 const StyleBackgroundImage = tw.div`
 bg-no-repeat
 bg-bottom
@@ -31,6 +41,8 @@ bg-cover
 max-w-full
 h-[640px]
 `;
+
+let id = "4aawyAB9vmqN3uQ7FjRGTy";
 
 function millisToMinutesAndSeconds(millis) {
   var minutes = Math.floor(millis / 60000);
@@ -47,7 +59,8 @@ const AlbumDetailsPage = () => {
     async function fetchDataFromSpotify() {
       try {
         const data = await fetchFromApi(
-          "https://api.spotify.com/v1/albums/4aawyAB9vmqN3uQ7FjRGTy"
+          { id }`https://api.spotify.com/v1/albums/${id}`
+          //  "https://api.spotify.com/v1/albums/4aawyAB9vmqN3uQ7FjRGTy"
         );
         if (data) {
           setAlbumDetails(data);
@@ -65,43 +78,81 @@ const AlbumDetailsPage = () => {
 
   return (
     <>
-      <Header />
-
-      {albumDetails.images && albumDetails.images.length > 0 && (
-        <StyleBackgroundImage
-          style={{
-            backgroundImage: `url(${albumDetails.images[0].url})`,
-          }}
-        ></StyleBackgroundImage>
-      )}
-
-      <p>{albumDetails.album_type}</p>
-      <p>{albumDetails.label}</p>
-
+      <section>
+        {albumDetails.images && albumDetails.images.length > 0 && (
+          <StyleBackgroundImage
+            style={{
+              backgroundImage: `url(${albumDetails.images[0].url})`,
+            }}
+          >
+            <Header className="text-white" />
+            <div className="px-6 pb-20">
+              <h1 className="text-white font-bold text-4xl pt-8 pb-7 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+                {albumDetails.name}
+              </h1>
+              <p className="text-white font-bold text-base drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+                {albumDetails.total_tracks + " songs"}
+              </p>
+              <div>
+                {" "}
+                {albumDetails.genres && albumDetails.genres.items
+                  ? albumDetails.genres.items.map((genres, index) => (
+                      <section
+                        className="flex justify-between max-w-lg"
+                        key={index}
+                      >
+                        {" "}
+                        <h4 className="pt-60 text-white font-extralight drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+                          {" "}
+                          genres hashtags
+                        </h4>
+                        <div className="flex gap-2 pt-3 text-white">
+                          <StyledButton> {track.genres[0]?.name}</StyledButton>
+                          <StyledButton>#country roads</StyledButton>
+                        </div>
+                      </section>
+                    ))
+                  : ""}
+              </div>{" "}
+              <h4 className="pt-60 text-white font-extralight drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+                {" "}
+                genres hashtags
+              </h4>
+              <div className="flex gap-2 pt-3 text-white">
+                <StyledButton> #country</StyledButton>
+                <StyledButton>#country roads</StyledButton>
+              </div>
+            </div>
+          </StyleBackgroundImage>
+        )}
+      </section>
       <main className="px-6 pb-20 grid-cols-4">
-        <h2>All Songs</h2>
+        <h2 className="font-poppins w-40 pt-10 pb-6  font-bold text-secondary-color">
+          All Songs
+        </h2>
         <ul>
           {albumDetails.tracks && albumDetails.tracks.items ? (
             albumDetails.tracks.items.map((track, index) => (
-              <li className="flex gap-4 justify-between" key={index}>
-                <div className="flex gap-4 py-2 items-center max-w-lg">
+              <li className="flex justify-between max-w-lg" key={index}>
+                <section className="flex gap-5 py-2 items-center">
                   {" "}
                   <StyledIconDiv>
                     <StyledPlayIcon />{" "}
                   </StyledIconDiv>
                   <div className="flex flex-col">
-                    <h3 className="font-poppins font-bold"> {track.name}</h3>
+                    <h3 className="font-poppins w-40 font-bold text-secondary-color">
+                      {" "}
+                      {track.name}
+                    </h3>
 
-                    <div>
-                      <p>
-                        {track.artists[0]?.name}
-                        {track.artists[1]?.name &&
-                          ` feat ${track.artists[1].name}`}
-                      </p>
-                    </div>
+                    <p className="max-w-full">
+                      {track.artists[0]?.name}
+                      {track.artists[1]?.name && ` feat. `}
+                    </p>
+                    <p> {track.artists[1]?.name}</p>
                   </div>
-                </div>
-                <p className="w-32  flex">
+                </section>
+                <p className="w-6 text-right flex">
                   {millisToMinutesAndSeconds(track.duration_ms, "s")} mins
                 </p>
               </li>
