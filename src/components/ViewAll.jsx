db60b9701b997f53c4d7617843b5e9a149d5abf2
mido <101448145+midoch@users.noreply.github.com>
2023-11-21 13:50:02 +0100

    Update ViewAll.jsx

--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -1,14 +1,11 @@
-// ViewAll.js
-
 import React from "react";
 
-const ViewAll = ({ text }) => {
+const ViewAll = ({ text, showButton }) => {
   return (
     <div className="flex justify-between items-center p-4">
       <p className="font-bold">{text}</p>
-      <button className="text-pink-500 px-4">View All</button>
+      {showButton && <button className="text-pink-500 px-4">View All</button>}
     </div>
   );
 };
-
 export default ViewAll;
