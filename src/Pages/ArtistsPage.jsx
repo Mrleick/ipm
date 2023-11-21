20127ce204c0f19770a300429dc8b5323ce172d6
mido <101448145+midoch@users.noreply.github.com>
2023-11-21 14:43:48 +0100

    Update ArtistsPage.jsx

--- src/Pages/ArtistsPage.jsx
+++ src/Pages/ArtistsPage.jsx
@@ -5,20 +5,20 @@ import Footer from "../Components/FooterMenu";
 import ArtistsOfTheMonth from "../components/ArtistsOfTheMonth";
 import ViewAll from "../components/ViewAll";
 import ArtistCard from "../components/ArtistCard";
+import TopArtistsByCountry from "../components/TopArtistsByCountry";
+import Heading from "../Components/Heading";
 
 const ArtistsPage = () => {
   return (
     <div>
       <Header className="uppercase tracking-wider text-black dark:text-white flex justify-between py-4 px-4 dark:bg-secondary-color" />
-
-      <h1 className="pl-4">Artists Page</h1>
+      <Heading className="pl-4" title="All Artists" />
       <ViewAll text="Artists of the Month" showButton={true} />
       <ArtistsOfTheMonth />
       <ViewAll text="Featured Artists" showButton={true} />
       <ArtistCard />
       <ViewAll text="Top Artists By Country" showButton={false} />
-      <ViewAll text="Top Artists By Country" showButton={false} />
-      <ViewAll text="Top Artists By Country" showButton={false} />
+      <TopArtistsByCountry country="DK" />
       <Footer />
     </div>
   );
