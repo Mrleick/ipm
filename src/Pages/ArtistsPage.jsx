fd117a4de8988e21994745d46ae91786f7018fe0
mido <101448145+midoch@users.noreply.github.com>
2023-11-21 13:46:20 +0100

    Update ArtistsPage.jsx

--- src/Pages/ArtistsPage.jsx
+++ src/Pages/ArtistsPage.jsx
@@ -1,9 +1,10 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Header from "../Components/Header";
 import Footer from "../Components/FooterMenu";
 import ArtistsOfTheMonth from "../components/ArtistsOfTheMonth";
 import ViewAll from "../components/ViewAll";
+import ArtistCard from "../components/ArtistCard";
 
 const ArtistsPage = () => {
   return (
@@ -13,6 +14,13 @@ const ArtistsPage = () => {
       <h1 className="pl-4">Artists Page</h1>
       <ViewAll text="Artists of the Month" />
       <ArtistsOfTheMonth />
+      <ViewAll text="Featured Artists" />
+      <ArtistCard />
+      <ViewAll text="Featured Artists" />
+      <ViewAll text="Featured Artists" />
+      <ViewAll text="Featured Artists" />
+      <ViewAll text="Featured Artists" />
+      <ViewAll text="Featured Artists" />
       <ViewAll text="Featured Artists" />
       <Footer />
     </div>
