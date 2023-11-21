29e36d94333da7c6c8390bf04a173a6f793c0301
mido <101448145+midoch@users.noreply.github.com>
2023-11-21 11:57:29 +0100

    Update ArtistsPage.jsx

--- src/Pages/ArtistsPage.jsx
+++ src/Pages/ArtistsPage.jsx
@@ -1,12 +1,22 @@
+import React from "react";
 import { Link } from "react-router-dom";
+import Header from "../Components/Header";
+import Footer from "../Components/FooterMenu";
+import ArtistsOfTheMonth from "../components/ArtistsOfTheMonth";
+import ViewAll from "../components/ViewAll";
 
-const AlbumsPage = () => {
+const ArtistsPage = () => {
   return (
-    <>
-      <h1>Albums</h1>
-      <Link to="/Albums">Albums</Link>
-    </>
+    <div>
+      <Header className="uppercase tracking-wider text-black dark:text-white flex justify-between py-4 px-4 dark:bg-secondary-color" />
+
+      <h1 className="pl-4">Artists Page</h1>
+      <ViewAll text="Artists of the Month" />
+      <ArtistsOfTheMonth />
+      <ViewAll text="Featured Artists" />
+      <Footer />
+    </div>
   );
 };
 
-export default AlbumsPage;
+export default ArtistsPage;
