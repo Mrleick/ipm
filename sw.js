af0283d33dbf9fcfe18882f31f44ecb48bf59ef4
NickBLau <114568331+NickBLau@users.noreply.github.com>
2023-11-23 11:13:14 +0100

    Added PWA

---
+++ sw.js
@@ -0,0 +1,19 @@
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("static").then((cache) => {
+      return cache.addAll([
+        "./",
+        "/src/index.css",
+        "/src/icons/android-launchericon-192-192.png",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
