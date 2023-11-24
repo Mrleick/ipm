1f13eea23b3956cc5bd51237c92e9928fadb8975
kpauner <kpauner@gmail.com>
2023-11-24 12:35:12 +0100

    Update ArtistsPage.jsx

--- src/Pages/ArtistsPage.jsx
+++ src/Pages/ArtistsPage.jsx
@@ -1,10 +1,10 @@
 import Heading from "../components/Heading";
-import React, { useState, useEffect } from "react";
-import Footer from "../Components/FooterMenu";
+import Footer from "../components/FooterMenu";
 import ArtistsOfTheMonth from "../components/ArtistsOfTheMonth";
 import ViewAll from "../components/ViewAll";
 import ArtistCard from "../components/ArtistCard";
 import TopArtistsByCountry from "../components/TopArtistsByCountry";
+import Header from "../components/Header";
 
 const ArtistsPage = () => {
   return (
