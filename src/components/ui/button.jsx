17da777945f11a5c0decf7fdb21f231f4c957a46
Mrleick <104718595+Mrleick@users.noreply.github.com>
2023-11-27 10:17:38 +0100

    Renamed button

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -1,10 +1,11 @@
 export default function Button({ title, color }) {
-  return (
-    <button
-      type="submit"
-      className={`uppercase p-5 my-6 rounded-full border-4 font-bold tracking-wide bg-white block w-full ${color}`}
-    >
-      {title}
-    </button>
-  );
-}
+    return (
+      <button
+        type="submit"
+        className={`uppercase p-5 my-6 rounded-full border-4 font-bold tracking-wide bg-white block w-full ${color}`}
+      >
+        {title}
+      </button>
+    );
+  }
+  
