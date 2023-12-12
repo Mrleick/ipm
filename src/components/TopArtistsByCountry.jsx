bfced2c2710db4333497e0588712f61abcaaf0c5
Mathiassren <110809330+Mathiassren@users.noreply.github.com>
2023-12-12 11:01:02 +0100

    changes

--- src/components/TopArtistsByCountry.jsx
+++ src/components/TopArtistsByCountry.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import fetchFromApi from "../lib/fetchFromApi";
 
+import fetchFromApi from "../lib/fetchFromApi";
 const TopArtistsByCountry = ({ country }) => {
   const [token, setToken] = useState();
   const [topPlaylists, setTopPlaylists] = useState([]);
