cc45c07bb2b2e6cc6f045a41320e1ee91220c871
NickBLau <114568331+NickBLau@users.noreply.github.com>
2023-12-14 12:14:48 +0100

    Added check if there is 1 or more songs

--- src/Pages/AlbumsPage.jsx
+++ src/Pages/AlbumsPage.jsx
@@ -6,7 +6,6 @@ import fetchFromApi from "../lib/fetchFromApi";
 const AlbumPage = () => {
   const [token, setToken] = useState();
   const [album, setAlbum] = useState();
-  const [loading, setLoading] = useState(true);
   const [songs, setSongs] = useState();
   const [FeatAlbums, setFeatAlbums] = useState();
 
@@ -37,7 +36,6 @@ const AlbumPage = () => {
         );
         if (data) {
           setFeatAlbums(data.albums.items);
-          setLoading(false);
           console.log(data.albums.items);
         }
       } catch (error) {
@@ -49,7 +47,7 @@ const AlbumPage = () => {
   }, []);
 
   return (
-    <div className="dark:text-white dark:bg-secondary-color">
+    <div className="dark:text-white ease-in duration-300 dark:bg-secondary-color">
       <Header
         className=""
         buttonClass=""
@@ -60,90 +58,76 @@ const AlbumPage = () => {
         textColor=""
       />
       <main>
-        <h1 className=" bg-gradient-to-r from-orange to-primarycolor text-transparent bg-clip-text text-5xl pl-5 py-7">
+        <h1 className=" bg-gradient-to-r ease-in duration-300 from-orange to-primarycolor text-transparent bg-clip-text text-5xl pl-5 py-7">
           All Albums
         </h1>
         <div className="flex justify-between px-5 pb-4 text-white dark:text-black">
-          <p className="text-black dark:text-white font-bold text-lg">
+          <p className="text-black  dark:text-white font-bold text-lg">
             Featured Albums
           </p>
-          <a href="#" className="no-underline dark:text-primarycolor">
+          <a href="#" className="no-underline text-primarycolor">
             View more
           </a>
         </div>
-        <section className="gap-5 flex overflow-x-auto px-5 pb-8">
-          {loading ? (
-            <p className="px-6">Loading Featured Albums...</p>
-          ) : (
-            FeatAlbums &&
+        <section className="gap-5 flex overflow-x-auto px-5 pb-8 ">
+          {FeatAlbums &&
             FeatAlbums.map((album) => (
               <Link to={`/albumDetails/${album.id}`} key={album.id}>
-                <div className="flex gap-5 mr-8">
+                <div className="flex gap-5 mr-8 ">
                   <img
                     src={album.images[0].url}
-                    className="rounded-lg h-32 min-w-fit mr-24"
-                    alt={album.name}
+                    className="rounded-lg h-32 min-w-fit mr-24 "
                   />
                 </div>
               </Link>
-            ))
-          )}
+            ))}
         </section>
+        <div className="flex dark:bg-secondary-color justify-between px-5 pb-4 text-white dark:text-black">
+          <p className="text-black mb-5  dark:text-white font-bold text-lg">
+            New Releases
+          </p>
+          <a href="#" className="no-underline text-primarycolor">
+            View more
+          </a>
+        </div>
+        <section className="mb-16 flex flex-col dark:bg-red-500">
+          {songs &&
+            songs.map((single) => (
+              <Link
+                className="dark:bg-secondary-color"
+                to={`/albumDetails/${single.id}`}
+                key={single.id}
+              >
+                <article className="flex px-6 justify-between items-center mb-6 text-black dark:text-white dark:bg-secondary-color">
+                  <div className="flex gap-3 dark:bg-secondary-color">
+                    <img
+                      src={single.images[0].url}
+                      className="h-14 flex m-auto items-center rounded-lg"
+                    />
+                    <span>
+                      <h3 className="font-poppins w-40 font-bold text-secondary-color  dark:text-white">
+                        {single.name.length > 15
+                          ? single.name.slice(0, 15) + "..."
+                          : single.name}
+                      </h3>
+                      <p>
+                        {single.artists[0]?.name.length > 15
+                          ? single.artists[0]?.name.slice(0, 15) + "..."
+                          : single.artists[0]?.name}
+                      </p>
+                    </span>
+                  </div>
+                  <p className="m-0 ">
+                    {single.total_tracks === 1
+                      ? single.total_tracks + " song"
+                      : single.total_tracks + " songs"}
+                  </p>
+                </article>
+              </Link>
+            ))}
+        </section>{" "}
       </main>
-      <div className="flex dark:bg-secondary-color justify-between px-5 pb-4 text-white dark:text-black">
-        <p className="text-black mb-5 dark:text-white font-bold text-lg">
-          New Releases
-        </p>
-        <a
-          href="#"
-          className="no-underline text-purple-700 dark:text-primarycolor"
-        >
-          View more
-        </a>
-      </div>
-      <section className="mb-16 flex flex-col dark:bg-secondary-color">
-        {loading ? (
-          <p className="px-6">Loading new releases...</p>
-        ) : (
-          songs &&
-          songs.map((single) => (
-            <Link
-              key={single.id}
-              className="dark:bg-secondary-color"
-              to={`/albumDetails/${single.id}`}
-            >
-              <article className="flex px-6 justify-between items-center mb-6 text-black dark:text-white dark:bg-secondary-color">
-                <div className="flex gap-3 dark:bg-secondary-color">
-                  <img
-                    src={single.images[0].url}
-                    className="h-14 flex m-auto items-center rounded-lg"
-                    alt={single.name}
-                  />
-                  <span>
-                    <h3 className="font-poppins w-40 font-bold text-secondary-color dark:text-white">
-                      {single.name.length > 15
-                        ? single.name.slice(0, 15) + "..."
-                        : single.name}
-                    </h3>
-                    <p>
-                      {single.artists[0]?.name.length > 15
-                        ? single.artists[0]?.name.slice(0, 15) + "..."
-                        : single.artists[0]?.name}
-                    </p>
-                  </span>
-                </div>
-
-                <p className="m-0">
-                  {single.total_tracks === 1
-                    ? single.total_tracks + " song"
-                    : single.total_tracks + " songs"}
-                </p>
-              </article>
-            </Link>
-          ))
-        )}
-      </section>
-      <FooterMenu />
+      <FooterMenu />{" "}
     </div>
   );
 };
