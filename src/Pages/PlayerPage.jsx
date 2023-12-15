ae6a277aa92de8c1dfdecb18ca72f3b3a5aa4410
Daniel Christiansen <117975547+XENONDK@users.noreply.github.com>
2023-12-15 13:02:23 +0100

    current track details
    on player page, added functionality to write out the current track, name, duration, and artist

--- src/Pages/PlayerPage.jsx
+++ src/Pages/PlayerPage.jsx
@@ -9,9 +9,13 @@ import {
   IoPlaySharp,
 } from "react-icons/io5";
 import { useOutletContext } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 const PlayerPage = () => {
   const {SPPlayer, setSPPlayer} = useOutletContext()
+  const [currentTrack, setCurrentTrack] = useState()
+  const [currentArtist, setCurrentArtist] = useState()
+  const [playtime, setPlaytime] = useState()
 
   function pausePlay () {
     SPPlayer && SPPlayer.togglePlay().then(() => console.log("yay"))
@@ -22,6 +26,21 @@ const PlayerPage = () => {
   function previous () {
     SPPlayer && SPPlayer.previousTrack().then(() => console.log("yay"))
   }
+  
+  useEffect(() => {
+   SPPlayer && SPPlayer.getCurrentState().then((state)=> {
+      console.log(state)
+       setCurrentTrack(state.track_window.current_track.name)
+       setCurrentArtist(state.track_window.current_track.artists[0].name)
+      setPlaytime(millisToMinutesAndSeconds(state.duration))
+    })
+  })
+
+  function millisToMinutesAndSeconds(millis) {
+    var minutes = Math.floor(millis / 60000);
+    var seconds = ((millis % 60000) / 1000).toFixed(0);
+    return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+  }
 
   return (
     <>
@@ -31,8 +50,8 @@ const PlayerPage = () => {
         <img className="p-0 z-10" src={Vinyl} alt="Record Cover" />
       </div>
       <div className="mt-28">
-        <h2 className="font-bold text-center">Don’t Call Me Up</h2>
-        <p className="text-center">Mabel</p>
+        <h2 className="font-bold text-center"> {currentTrack && currentTrack} </h2>
+        <p className="text-center"> {currentArtist && currentArtist} </p>
       </div>
       <div className="flex justify-center items-center mt-4">
         <input
@@ -44,7 +63,7 @@ const PlayerPage = () => {
       </div>
       <div className="flex justify-between mx-auto mt-[15px] mb-[15px] max-w-[325px]">
         <p>00:00</p>
-        <p>3:40</p>
+        <p> {playtime ? playtime : "00:00"} </p>
       </div>
 
       <div className="flex gap-x-4 justify-center items-center">
