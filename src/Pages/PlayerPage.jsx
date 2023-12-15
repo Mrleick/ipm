e1310b971650cd7522ee0b8cffa5b870a87b53ab
NickBLau <114568331+NickBLau@users.noreply.github.com>
2023-12-15 14:12:02 +0100

    Added error playimte back to 00:00

--- src/Pages/PlayerPage.jsx
+++ src/Pages/PlayerPage.jsx
@@ -32,65 +32,56 @@ const PlayerPage = () => {
 
   return (
     <>
-      <div className="dark:text-white ease-in duration-300 dark:bg-secondary-color">
-        <Header
-          className=""
-          buttonClass=""
-          showBackButton={true}
-          showSearchButton={true}
-          isDarkMode={true}
-          showPageName={true}
-          textColor=""
+      <Header className="uppercase  text-black dark:text-white flex justify-between py-6 px-6 dark:bg-secondary-color  font-extralight" />
+      <div className="flex justify-center items-center mx-auto mt-[64px] w-full relative">
+        <img className="p-0 absolute w-[375px]" src={Wave} alt="Sound Wave" />
+        <img className="p-0 z-10" src={Vinyl} alt="Record Cover" />
+      </div>
+      <div className="mt-28">
+        <h2 className="font-bold text-center">
+          {currentTrack ? currentTrack : "Unknown Track"}
+        </h2>
+        <p className="text-center">
+          {currentArtist ? currentArtist : "Unknown Artist"}
+        </p>
+      </div>
+      <div className="flex justify-center items-center mt-4">
+        <input
+          type="range"
+          min="0"
+          max="100"
+          className="w-[325px] appearance-none bg-[#FF1168] h-1 outline-none"
         />
-        <div className="flex justify-center items-center mx-auto mt-[64px] w-full relative">
-          <img className="p-0 absolute w-[375px]" src={Wave} alt="Sound Wave" />
-          <div className={`p-0 z-10 ${isPlaying ? "animate-spin" : ""}`}>
-            <img src={Vinyl} alt="Record Cover" />
-          </div>
-        </div>
-        <div className="mt-28">
-          <h2 className="font-bold text-center">Don’t Call Me Up</h2>
-          <p className="text-center">Mabel</p>
-        </div>
-        <div className="flex justify-center items-center mt-4">
-          <input
-            type="range"
-            min="0"
-            max="100"
-            className="w-[325px] appearance-none bg-[#FF1168] h-1 outline-none"
-          />
-        </div>
-        <div className="flex justify-between mx-auto mt-[15px] mb-[15px] max-w-[325px]">
-          <p>00:00</p>
-          <p>3:40</p>
-        </div>
+      </div>
+      <div className="flex justify-between mx-auto mt-[15px] mb-[15px] max-w-[325px]">
+        <p>00:00</p>
+        <p> {playtime ? playtime : "00:00"} </p>
+      </div>
 
-        <div className="flex gap-x-4 justify-center pb-4 items-center">
-          <button onClick={previous}>
-            <img src={Backward} alt="Backward" />
-          </button>
+      <div className="flex gap-x-4 justify-center items-center">
+        <button onClick={previous}>
+          <img src={Backward} alt="Backward" />
+        </button>
 
-          <button id="seekback">
-            <IoPlayBackSharp className="text-4xl" />
-          </button>
+        <button id="seekback">
+          <IoPlayBackSharp className="text-4xl" />
+        </button>
 
-          <button
-            onClick={pausePlay}
-            className="flex justify-center items-center h-[75px] w-[75px] rounded-full bg-gradient-to-r from-orange to-primarycolor"
-          >
-            <IoPlaySharp className={`text-white text-[40px]`} />
-          </button>
+        <button
+          onClick={pausePlay}
+          className="flex justify-center items-center h-[75px] w-[75px] rounded-full bg-gradient-to-r from-orange to-primarycolor"
+        >
+          <IoPlaySharp className="text-white text-[40px]" />
+        </button>
 
-          <IoPlayForwardSharp className="text-4xl" />
+        <IoPlayForwardSharp className="text-4xl" />
 
-          <button onClick={next}>
-            <img src={Forward} alt="Forward" />
-          </button>
-        </div>
+        <button onClick={next}>
+          <img src={Forward} alt="Forward" />
+        </button>
       </div>
     </>
   );
 };
 
 export default PlayerPage;
-
